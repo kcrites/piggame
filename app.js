df3c12f8913fad7ad9f5c4fe9e8f34a8dc2ca4e8
Ken Crites <kenrcrites@gmail.com>
2019-04-26 11:10:23 +0200

    Added 2nd rule to game

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ GAME RULES:
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
-1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
--2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
+1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. 
+2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
 -3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
 */
 
-var scores, roundScore, activePlayer, gamePlaying, prevRoll;
+var scores, roundScore, activePlayer, gamePlaying, prevRoll, topScore;
 
 init();
 
@@ -26,7 +26,6 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 			//add score
 			roundScore += dice;
 			document.querySelector('#current-' + activePlayer).textContent = roundScore;
-			console.log(`prev: ${prevRoll}, current: ${dice}`);
 			if(prevRoll === 6 && dice === 6) {
 				//Player loses entire score
 				scores[activePlayer] = 0;
@@ -52,7 +51,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 		document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 		
 		//check to see if player won the game
-		if(scores[activePlayer] >= 20){
+		if(scores[activePlayer] >= topScore){
 			document.querySelector('#name-' + activePlayer).textContent = "Winner!";
 			document.querySelector('.dice').style.display = 'none';
 			document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
@@ -82,6 +81,16 @@ const nextPlayer = () => {
 }
 
 document.querySelector('.btn-new').addEventListener('click', init);
+document.querySelector('.btn-submit').addEventListener('click', function() {
+	var a = document.getElementById("newscore").value;
+	a = Number(a);
+	if(a > 0){
+		topScore = a;
+		document.getElementById('winning-score').textContent = topScore;
+		document.getElementById("newscore").value = '';
+	}
+});
+
 
 function init()  {
 	scores= [0,0];
@@ -89,6 +98,7 @@ function init()  {
 	roundScore = 0;
 	gamePlaying = true;
 	prevRoll = 0;
+	topScore = 20;
 	document.querySelector('.dice').style.display = 'none';
 	document.getElementById('score-0').textContent = '0';
 	document.getElementById('score-1').textContent = '0';
@@ -96,10 +106,12 @@ function init()  {
 	document.getElementById('current-1').textContent = '0';
 	document.getElementById('name-0').textContent = "Player 1";
 	document.getElementById('name-1').textContent = "Player 2";
+	document.getElementById('winning-score').textContent = topScore;
 	document.querySelector('.player-0-panel').classList.remove('winner');
 	document.querySelector('.player-1-panel').classList.remove('winner');
 	document.querySelector('.player-0-panel').classList.remove('active');
 	document.querySelector('.player-1-panel').classList.remove('active');
 	document.querySelector('.player-0-panel').classList.add('active');
+	document.getElementById("newscore").value = '';
 
 }
